eae92735081fc056ab4f3c7ab5ba4e75535d3790
luis zuniga <lufezuro@gmail.com>
2019-06-17 15:18:41 -0500

    test works

--- test/test-cotizacion.py
+++ test/test-cotizacion.py
@@ -68,7 +68,7 @@ class Test(unittest.TestCase):
 
 	def test_cotizar_seguro_valor_adicional_hombres_jovenes_y_soltero(self):
 		valor = cot.cotizar_seguro("Guayaquil", 18, "hombre", "soltero", "", 0)
-		self.assertEqual(valor,'El valor calculado de su cotización es de 90.00')
+		self.assertEqual(valor,'El valor calculado de su cotización es de 70.00')
 
 	def test_cotizar_seguro_valor_adicional_mujer_con_osteoporosis(self):
 		valor = cot.cotizar_seguro("Guayaquil", 41, "mujer", "soltero", "osteoporosis", 0)
